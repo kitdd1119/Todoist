d8a27b76f41d262e5845a39aa8c055b4edd92210
kitdd1119 <kitdd1119@gmail.com>
2024-05-11 23:08:49 +0900

    화면을 관리하는 내비게이터 구조부터 수정할 예정

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Image, StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 
 import TodayScreen from './screens/TodayScreen';
@@ -13,7 +14,119 @@ import ScheduleInput from './components/schedule/ScheduleInput';
 import ScheduleAddButton from './components/schedule/ScheduleAddButton';
 import TopOption from './components/topOption/TopOption';
 
+const Stack = createNativeStackNavigator();
 const BottomTab = createBottomTabNavigator();
+
+function MainOverview() {
+  const [courseSchedules, setCourseSchedules] = useState([]);
+
+  return (
+    <BottomTab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: '#f9f9f2',
+          borderTopWidth: 0,
+        },
+        tabBarActiveTintColor: Colors.mainColor,
+      }}
+    >
+      <BottomTab.Screen
+        name="오늘 화면"
+        options={{
+          headerShown: false,
+          tabBarLabel: '오늘',
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused
+                ? require('./assets/BottomTab/Today2.png')
+                : require('./assets/BottomTab/Today.png')}
+              style={{ width: 25, height: 25 }}
+            />
+          )
+        }}
+      >
+        {() => (
+          <>
+            <SafeAreaView style={styles.screen}>
+              <View style={styles.TopOption}>
+                <TopOption todayScreen={true} />
+              </View>
+              <TodayScreen
+                courseSchedules={courseSchedules}
+                setCourseSchedules={setCourseSchedules}
+              />
+            </SafeAreaView>
+          </>
+        )}
+      </BottomTab.Screen>
+      <BottomTab.Screen
+        name="관리함 화면"
+        options={{
+          headerShown: false,
+          tabBarLabel: '관리함',
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused
+                ? require('./assets/BottomTab/ManagementBox2.png')
+                : require('./assets/BottomTab/ManagementBox.png')}
+              style={{ width: 25, height: 25 }}
+            />
+          )
+        }}
+      >
+        {() => (
+          <>
+            <View style={styles.TopOption}>
+              <TopOption todayScreen={false} />
+            </View>
+            <ManagementBoxScreen
+              courseSchedules={courseSchedules}
+              setCourseSchedules={setCourseSchedules}
+            />
+          </>
+        )}
+      </BottomTab.Screen>
+      <BottomTab.Screen
+        name="검색 화면"
+        options={{
+          headerShown: false,
+          tabBarLabel: '검색',
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused
+                ? require('./assets/BottomTab/Search2.png')
+                : require('./assets/BottomTab/Search.png')}
+              style={{ width: 25, height: 25 }}
+            />
+          )
+        }}
+      >
+        {() => (
+          <SearchScreen
+            courseSchedules={courseSchedules}
+            setCourseSchedules={setCourseSchedules}
+          />
+        )}
+      </BottomTab.Screen>
+      <BottomTab.Screen
+        name="목록 화면"
+        component={ListScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: '목록',
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused
+                ? require('./assets/BottomTab/List2.png')
+                : require('./assets/BottomTab/List.png')}
+              style={{ width: 25, height: 25 }}
+            />
+          )
+        }}
+      />
+    </BottomTab.Navigator>
+  )
+}
 
 export default function App() {
 
@@ -36,117 +149,27 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
+    <>
       <StatusBar style='dark' />
-      <BottomTab.Navigator
-        screenOptions={{
-          tabBarStyle: {
-            backgroundColor: '#f9f9f2',
-            borderTopWidth: 0,
-          },
-          tabBarActiveTintColor: Colors.mainColor,
-        }}
-      >
-        <BottomTab.Screen
-          name="오늘 화면"
-          options={{
-            headerShown: false,
-            tabBarLabel: '오늘',
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={focused
-                  ? require('./assets/BottomTab/Today2.png')
-                  : require('./assets/BottomTab/Today.png')}
-                style={{ width: 25, height: 25 }}
-              />
-            )
-          }}
-        >
-          {() => (
-            <>
-              <View style={styles.TopOption}>
-                <TopOption todayScreen={true} />
-              </View>
-              <TodayScreen
-                courseSchedules={courseSchedules}
-                setCourseSchedules={setCourseSchedules}
-              />
-            </>
-          )}
-        </BottomTab.Screen>
-        <BottomTab.Screen
-          name="관리함 화면"
-          options={{
-            headerShown: false,
-            tabBarLabel: '관리함',
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={focused
-                  ? require('./assets/BottomTab/ManagementBox2.png')
-                  : require('./assets/BottomTab/ManagementBox.png')}
-                style={{ width: 25, height: 25 }}
-              />
-            )
-          }}
-        >
-          {() => (
-            <>
-              <View style={styles.TopOption}>
-                <TopOption todayScreen={false} />
-              </View>
-              <ManagementBoxScreen
-                courseSchedules={courseSchedules}
-                setCourseSchedules={setCourseSchedules}
-              />
-            </>
-          )}
-        </BottomTab.Screen>
-        <BottomTab.Screen
-          name="검색 화면"
-          options={{
-            headerShown: false,
-            tabBarLabel: '검색',
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={focused
-                  ? require('./assets/BottomTab/Search2.png')
-                  : require('./assets/BottomTab/Search.png')}
-                style={{ width: 25, height: 25 }}
-              />
-            )
-          }}
-        >
-          {() => (
-            <SearchScreen
-              courseSchedules={courseSchedules}
-              setCourseSchedules={setCourseSchedules}
-            />
-          )}
-        </BottomTab.Screen>
-        <BottomTab.Screen
-          name="목록 화면"
-          component={ListScreen}
-          options={{
-            headerShown: false,
-            tabBarLabel: '목록',
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={focused
-                  ? require('./assets/BottomTab/List2.png')
-                  : require('./assets/BottomTab/List.png')}
-                style={{ width: 25, height: 25 }}
-              />
-            )
-          }}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='MainOverview'
+            component={MainOverview}
+            options={{
+              headerShown: false
+            }}
+          />
+          {/* <Stack.Screen /> */}
+        </Stack.Navigator>
+        <ScheduleInput
+          visible={modalIsVisible}
+          onAddSchedule={addScheduleHandler}
+          offSchedule={endScheduleAddButtonHandler}
         />
-      </BottomTab.Navigator>
-      <ScheduleInput
-        visible={modalIsVisible}
-        onAddSchedule={addScheduleHandler}
-        offSchedule={endScheduleAddButtonHandler}
-      />
-      <ScheduleAddButton onModal={startScheduleAddButtonHandler} />
-    </NavigationContainer>
+        <ScheduleAddButton onModal={startScheduleAddButtonHandler} />
+      </NavigationContainer>
+    </>
   );
 }
 
@@ -156,5 +179,5 @@ const styles = StyleSheet.create({
     top: 32,
     right: 10,
     zIndex: 1,
-},
+  },
 });
